c2f2cbfa5b79e72da70c62735f06197dab5fd623
MaduraMadushan <39506813+MaduraMadushan@users.noreply.github.com>
2019-01-08 06:07:41 +0530

    URL Parameters

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -15,7 +15,6 @@ const AppRoter = () => (
             <Switch>
                 <Route path="/" component={ExpenseDashboardPage} exact={true}/>
                 <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit" component={EditExpensePage} />
                 <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage} />
             </Switch>
